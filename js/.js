9b35d00cb07e32a05ef94ccd877501ac27530fa4
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-01-26 19:56:01 -0800

    implemeted siple routing

---
+++ js/.js
@@ -0,0 +1,27 @@
+var app = angular.module('app', ['ngRoute']);
+
+app.controller('myController', function($scope){
+
+});
+
+
+app.config(function($routeProvider){
+  $routeProvider
+  .when('/',{
+    templateUrl: 'partials/home.html'),
+    controller: "HomeController"
+    }
+    .when('/dogs', {
+      templateUrl: 'partials/dogs.html',
+      controller: 'DdogsController'
+    })
+});
+
+
+app.controller('HomeController', function($scope){
+  console.log('HomeController');
+});
+
+app.controller('DogsController', function($scope){
+  console.log('Dogs Controllers');
+});
